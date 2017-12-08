311f82a496d19764f304edd7740341d807b9eb98
Guest User <Guest@Epicodus-8C.local>
2017-12-08 10:48:35 -0800

    set up general rules in js

--- js/scripts.js
+++ js/scripts.js
@@ -4,18 +4,26 @@ $(document).ready(function() {
     var input = parseInt($("input#words").val());
     var result = [];
     debugger;
+    var beep = "Beep!"
+    var boop = "Boop!"
+    var dave = "I'm sorry, Dave. I'm afraid I can't do that."
     console.log(input);
 
 
 
-
-
-
-
       for (var i=0; i<=input; i++) {
-        result.push(i);
-        console.log(i);
-      $("#result").text(result);
+        if (result.charAt() === 1) {
+          result.push(beep)
+        } if (result.charAt() === 0) {
+          result.push(boop)
+        } else if (result % 3 === 0) {
+          result.push(dave)
+        } else {
+          result.push(i);
+          break;
+        }
+
+      $("#result").text(resultsequence);
   };
 });
 });
