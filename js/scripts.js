bc70616f341899509ed14102461787ee1cd31010
Guest User <Guest@Epicodus-8C.local>
2017-12-08 12:52:10 -0800

    division will work and return dave

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,14 @@
 //backend
+var beep = "Beep!";
+var boop = "Boop!";
+var dave = "I'm sorry, Dave. I'm afraid I can't do that.";
 var counter = function(input) {
-  for (var i=0; i<=input; i++) {
-    if (i % 3 === 0){
+    if (input % 3 === 0){
       return dave;
     } else {
       return false;
     }
-
-
-  result.push(i);
-  var beep = "Beep!";
-  var boop = "Boop!";
-  var dave = "I'm sorry, Dave. I'm afraid I can't do that.";
   }
-}
 // if (result.charAt(i) === 1) {
 //   result.replace(beep);
 // } if (result.charAt(i) === 0) {
@@ -36,11 +31,10 @@ $(document).ready(function() {
     var input = parseInt($("input#number").val());
     var result = counter(input);
     debugger;
-
     if (!result) {
       console.log("keep working");
     } else {
-      console.log("true");
+      console.log(dave);
     }
       $("#result").text(result);
   });
