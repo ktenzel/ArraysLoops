38233adc8ff512ce58191c9e329a347043cc5061
Guest User <Guest@Epicodus-8C.local>
2017-12-08 16:17:52 -0800

    adds beep and dave functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,30 @@
 //backend
-var beep = "Beep!";
-var boop = "Boop!";
-var dave = "I'm sorry, Dave. I'm afraid I can't do that.";
 var array = []
 // var inputarray = input.split('');
 
 var counter = function(input) {
-  for (var i=0; i<=input; i++)
-  array.push(i);
+  for (var i=0; i<=input; i++) {
+    array.push(i);
     if (array[i] % 3 === 0) {
-    array[i] = dave;
+    array[i] = "I'm sorry, Dave. I'm afraid I can't do that.";
     console.log(i);
+  } if (array[i] % 10 === 0) {
+    array[i] = "Beep!"}
+  // } else if (array.forEach(array, 1)){
+  //   array[i] = "Boop!"
+  // }
   }
 }
-if (result.charAt(i) === 1) {
-  result.replace(beep);
-} if (result.charAt(i) === 0) {
-  result.replaces(boop);
-} else if (result % 3 === 0) {
-  result.replace(dave);
-} else {
-  result.push(i);
-  break;
-}
+// if (result.charAt(i) === 1) {
+//   result.replace(beep);
+// } if (result.charAt(i) === 0) {
+//   result.replaces(boop);
+// } else if (result % 3 === 0) {
+//   result.replace(dave);
+// } else {
+//   result.push(i);
+//   break;
+// }
 
 
 
