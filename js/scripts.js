2f786b921f1105a2e43f830316fc6686f4ab4691
ktenzel <ktenzel@gmail.com>
2017-12-14 14:21:36 -0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,45 @@
 //backend
 var array = []
-var stringarray = array.toString();
-// var inputarray = input.split('');
 
 var counter = function(input, name) {
   for (var i=0; i<=input; i++) {
     array.push(i);
-    if (array[i] % 3 === 0) {
-    array[i] = "I'm sorry, Dave. I'm afraid I can't do that.";
+    if (array[i] % 10 === 0) {
+    array[i] = "Beep!";
+    $("#result-list").append("<li>" + array[i] + "</li>")
     console.log(i);
-  } if (array[i] % 10 === 0) {
-    array[i] = "Beep!"
-  } if (array[i] % 10 === 1) {
-      array[i] = "Boop!"
-    }
+  } else if (array[i] % 10 === 1) {
+    array[i] = "Boop!";
+    $("#result-list").append("<li>" + array[i] + "</li>")
+  } else if (array[i] % 3 === 0) {
+      array[i] = "I'm sorry, Dave. I'm afraid I can't do that.";
+      $("#result-list").append("<li>" + array[i] + "</li>")
+  } else if (i.toString().includes("0")) {
+    array[i] = "Beep!";
+    $("#result-list").append("<li>" + array[i] + "</li>")
+  } else if (i.toString().includes("1")) {
+    array[i] = "Boop!";
+    $("#result-list").append("<li>" + array[i] + "</li>")
+  } else {
+    $("#result-list").append("<li>" + array[i] + "</li>")
+  }
   }
 }
-// if (result.charAt(i) === 1) {
-//   result.replace(beep);
-// } if (result.charAt(i) === 0) {
-//   result.replaces(boop);
-// } else if (result % 3 === 0) {
-//   result.replace(dave);
-// } else {
-//   result.push(i);
-//   break;
-// }
-
-
-
-
 
 //frontend
 $(document).ready(function() {
   $("form#evaluate").submit(function(event) {
     var input = parseInt($("input#number").val());
     event.preventDefault();
-    // var name = $("input#name").val()
     var result = counter(input);
     debugger;
-    // if (!result) {
-    //   console.log("keep working");
-    // } else {
-    //   console.log(dave);
-    // }
       $("#result").show();
-      $(".contain").text(array);
+
+      var listArray = function() {
+        for(i = 0; i < array.length; i++) {
+          $("#result-list").append("<li>" + array[i] + "</li>");
+        }
+    }
+
   });
 });
